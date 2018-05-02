15773c9d7dcde5fe458da895871da26d9fd17c7c
sultankadyr <sultan.kadyrkesh@gmail.com>
2018-05-03 05:00:34 +0600

    first commit

---
+++ TexerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TexerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
